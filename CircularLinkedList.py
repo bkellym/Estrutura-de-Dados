97ad759904160fcb6526fd08435ecaaddf5179e6
Keller <bkelly.sm@gmail.com>
2020-08-22 10:20:56 -0300

    Estruturação de Métodos das Classes

--- CircularLinkedList.py
+++ CircularLinkedList.py
@@ -7,4 +7,26 @@ class CircularLinkedList:
         self.head: Node = None
         self.tail: Node = None
 
-    # TODO: Desenvolver Classe
+    def list_print(self):
+        # TODO: Método de listar valores - Print
+        return
+
+    def show_slide(self):
+        # TODO: Método de listar valores - OpenCV
+        return
+
+    def at_start(self, value):
+        # TODO: Método inserir Node início da lista
+        return
+
+    def at_end(self, value):
+        # TODO: Método inserir Node final da lista
+        return
+
+    def at_between(self, value, position):
+        # TODO: Método inserir Node na posição informada
+        return
+
+    def remove(self, value):
+        # TODO: Método remover Node que armazene valor informado
+        return
--- LinkedList.py
+++ LinkedList.py
@@ -4,6 +4,28 @@ from Node import *
 class LinkedList:
 
     def __init__(self):
-        self.head: Node = None
+        self.head = None
 
-    # TODO: Desenvolver Classe
+    def list_print(self):
+        # TODO: Método de listar valores - Print
+        return
+
+    def show_slide(self):
+        # TODO: Método de listar valores - OpenCV
+        return
+
+    def at_start(self, value):
+        # TODO: Método inserir Node início da lista
+        return
+
+    def at_end(self, value):
+        # TODO: Método inserir Node final da lista
+        return
+
+    def at_between(self, value, position):
+        # TODO: Método inserir Node na posição informada
+        return
+
+    def remove(self, value):
+        # TODO: Método remover Node que armazene valor informado
+        return
