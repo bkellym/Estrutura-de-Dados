d71925260e1876754e0a90abf7ececa028de15c9
Keller <bkelly.sm@gmail.com>
2020-08-26 02:19:01 -0300

    Corrige teste: valor não existente ao remover
    Descrição: Quando o valor informado não existe, a lista apenas não sobre alteração.
    Não dispara erro, pois isto impossibilitaria a recursão do método.

--- test/CircularLinkedListTest.py
+++ test/CircularLinkedListTest.py
@@ -201,8 +201,8 @@ class MyTestCase(unittest.TestCase):
         lista = CircularLinkedList()
         lista.at_start(0)
         lista.at_end(1)
-        with self.assertRaisesWithMessage(ValueError):
-            lista.remove(3)
+        lista.remove(3)
+        self.assertEqual(lista.list_print(), [0, 1])
 
     # Utility functions
     def assertRaisesWithMessage(self, exception):
